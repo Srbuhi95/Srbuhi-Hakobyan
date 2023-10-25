c13bcb646d41e99e7494be2d480873dca6153fb2
Srbuhi95 <srbuhih2@gmail.com>
2023-10-25 17:21:12 +0400

    new cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -2,13 +2,9 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
-
-module.exports = {
-  baseUrl: "https://coinstats.app", // Replace with the actual base URL of your application
-  integrationFolder: "cypress/integration", // Folder where your test files are located
-};
