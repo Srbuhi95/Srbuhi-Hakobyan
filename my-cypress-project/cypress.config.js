01884ffcf2f18c04dfa16e2018de6cc72d9608cc
Srbuhi95 <srbuhih2@gmail.com>
2023-10-25 15:40:20 +0400

    add cypress config

--- my-cypress-project/cypress.config.js
+++ my-cypress-project/cypress.config.js
@@ -7,6 +7,7 @@ module.exports = defineConfig({
     },
   },
 });
+
 module.exports = {
   baseUrl: "https://coinstats.app", // Replace with the actual base URL of your application
   integrationFolder: "cypress/integration", // Folder where your test files are located
