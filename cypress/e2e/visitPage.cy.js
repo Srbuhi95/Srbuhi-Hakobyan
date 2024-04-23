55c1f533d6be7bcdaec3caf34dc79c522de36b67
Srbuhi95 <srbuhih2@gmail.com>
2024-04-23 15:32:09 +0400

    Change the title

--- cypress/e2e/visitPage.cy.js
+++ cypress/e2e/visitPage.cy.js
@@ -2,6 +2,5 @@ describe("Visit CoinStats.App", () => {
     it("Visits the CoinStats website", () => {
         cy.visit("https://coinstats.app");
         cy.contains("Manage All Your Wallets & Exchanges From One Place");
-        cy.contains("Manage All Yoom One Place");
     });
 });
